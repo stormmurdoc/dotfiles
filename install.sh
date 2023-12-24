1e726c9a1e51fdb7ba8c12bc39d105015da5a56f
stormmurdoc <murdoc@storm-clan.de>
2023-12-24 11:34:22 +0100

    chore: testing nvim

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 WD=$(pwd)
 echo "Setup dotfiles"
 ln -svf "$WD"/alias/alias ~/.alias
-ln -svf "$WD"/nvim/lua/custom ~/.config/nvim/lua/
+#ln -svf "$WD"/nvim/lua/custom ~/.config/nvim/lua/
 #ln -svf "$WD"/zsh/zshrc ~/.zshr
