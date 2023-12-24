d30d5a169b970cf0156e9b2717e9b1e2ff0a214a
stormmurdoc <murdoc@storm-clan.de>
2023-12-24 10:57:04 +0100

    chore: changed nvim path #3

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ WD=$(pwd)
 echo "Setup dotfiles"
 ln -svf "$WD"/alias/alias ~/.alias
 
-if [ -r "~/.config/nvim" ];then
-    rm -rf " ~/.config/nvim"
-fi
+rm -rf " ~/.config/nvim"
 ln -svf "$WD"/nvim/ ~/.config/
 #ln -svf "$WD"/zsh/zshrc ~/.zshr
