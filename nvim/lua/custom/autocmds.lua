d7e0a9d2638a3333b218297d79d76360f500420b
stormmurdoc <murdoc@storm-clan.de>
2023-12-25 21:56:49 +0100

    chore: added function relative/absolute numbering

--- nvim/lua/custom/autocmds.lua
+++ nvim/lua/custom/autocmds.lua
@@ -9,3 +9,27 @@ local function liveReload_xresources()
     string.format "silent !xrdb merge ~/.Xresources && kill -USR1 $(xprop -id $(xdotool getwindowfocus) | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$')"
   )
 end
+
+local numtogGrp = vim.api.nvim_create_augroup("NumberToggle", { clear = true })
+vim.api.nvim_create_autocmd(
+  { "BufEnter", "InsertLeave", "FocusGained"},
+  { pattern = "*",
+    --command = "set relativenumber",  -- if callback, vim.api.nvim_buf_set_option(0,relativenumber,true) ?
+    callback = function()
+      vim.api.nvim_buf_set_option(0, 'relativenumber', true)
+    end,
+    group = numtogGrp,
+    desc = "Turn on relative line numbering when the buffer is entered.",
+  }
+)
+vim.api.nvim_create_autocmd(
+  { "BufLeave", "InsertEnter", "FocusLost"},
+  { pattern = "*",
+    --command = "set norelativenumber",
+    callback = function()
+      vim.api.nvim_buf_set_option(0, 'relativenumber', false)
+    end,
+    group = numtogGrp,
+    desc = "Turn off relative line numbering when the buffer is exited.",
+  }
+)
