1be2d70f058569ed96a8f9007aaf12fb95b23345
stormmurdoc <murdoc@storm-clan.de>
2023-12-25 22:02:51 +0100

    chore: commented out autocmd rel./abs. numbers

--- nvim/lua/custom/autocmds.lua
+++ nvim/lua/custom/autocmds.lua
@@ -10,26 +10,26 @@ local function liveReload_xresources()
   )
 end
 
-local numtogGrp = vim.api.nvim_create_augroup("NumberToggle", { clear = true })
-vim.api.nvim_create_autocmd(
-  { "BufEnter", "InsertLeave", "FocusGained"},
-  { pattern = "*",
-    --command = "set relativenumber",  -- if callback, vim.api.nvim_buf_set_option(0,relativenumber,true) ?
-    callback = function()
-      vim.wo.relativenumber = true
-    end,
-    group = numtogGrp,
-    desc = "Turn on relative line numbering when the buffer is entered.",
-  }
-)
-vim.api.nvim_create_autocmd(
-  { "BufLeave", "InsertEnter", "FocusLost"},
-  { pattern = "*",
-    --command = "set norelativenumber",
-    callback = function()
-      vim.wo.relativenumber = false
-    end,
-    group = numtogGrp,
-    desc = "Turn off relative line numbering when the buffer is exited.",
-  }
-)
+-- local numtogGrp = vim.api.nvim_create_augroup("NumberToggle", { clear = true })
+-- vim.api.nvim_create_autocmd(
+--   { "BufEnter", "InsertLeave", "FocusGained"},
+--   { pattern = "*",
+--     --command = "set relativenumber",
+--     callback = function()
+--       vim.wo.relativenumber = true
+--     end,
+--     group = numtogGrp,
+--     desc = "Turn on relative line numbering when the buffer is entered.",
+--   }
+-- )
+-- vim.api.nvim_create_autocmd(
+--   { "BufLeave", "InsertEnter", "FocusLost"},
+--   { pattern = "*",
+--     --command = "set norelativenumber",
+--     callback = function()
+--       vim.wo.relativenumber = false
+--     end,
+--     group = numtogGrp,
+--     desc = "Turn off relative line numbering when the buffer is exited.",
+--   }
+-- )
